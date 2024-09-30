3ef00246bff1a2d25cc8d79c95822653030c01a5
dsmundhe <dipakmundhe2026@gmail.com>
2024-09-30 06:37:07 -0700

    about change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,9 +8,11 @@ function About() {
         <div className="about-content">
           <h2>About Us</h2>
           <p>
-            We are <span>Aashvi Tekade</span> and <span>Dipak Mundhe</span>, students of the C-Tech Department at <span>YCCE Nagpur</span>. 
-            This webpage is our project for the Web Development Minor, designed to showcase our skills and earn 5 marks in the TAE-2 assessment.
+            We the students of <span> C-Tech Department</span>,  <span>Aashvi Tekade</span> and  <span>Dipak Munde </span>at  <span>YCCE Nagpur </span> 
+            This webpage is our project for the Web Development Minor, designed to showcase our skills.
+            Web development involves the creation and maintenance of websites. It encompasses several aspects, including web design, web publishing, web programming, and database management. Our project demonstrates the use of HTML, CSS, and JavaScript to create an interactive and user-friendly experience.
           </p>
+
         </div>
       </section>
     </div>
